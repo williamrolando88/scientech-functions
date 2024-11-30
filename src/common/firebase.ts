13b0ee295ebe8e9a4722590120416e42953ba96d
William Morales <william.rolando88@hotmail.com>
2024-11-29 21:26:53 -0500

    Add firestore test function

---
+++ src/common/firebase.ts
@@ -0,0 +1,5 @@
+import { initializeApp } from 'firebase-admin';
+
+const app = initializeApp();
+
+export const db = app.firestore();
--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,36 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onRequest } from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import * as logger from 'firebase-functions/logger';
+import { onDocumentWritten } from 'firebase-functions/v2/firestore';
+import { onRequest } from 'firebase-functions/v2/https';
+import { db } from './common/firebase';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
 });
+
+export const onInvoiceWritten = onDocumentWritten(
+  'invoices/{invoiceId}',
+  async (event) => {
+    const afterInvoice = event.data?.after.data();
+
+    const beforeInvoice = event.data?.before.data();
+
+    logger.info('Invoice written', {
+      afterInvoice,
+      beforeInvoice,
+    });
+
+    const categoriesSnapshot = await db
+      .collection('applicationSettings')
+      .doc('accountCategories')
+      .get();
+
+    logger.info('Account categories', categoriesSnapshot.data());
+  }
+);
