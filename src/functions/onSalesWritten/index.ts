a07a23780e5c49971733848fb9de5f8443d26bbd
William Morales <william.rolando88@hotmail.com>
2024-12-30 08:51:55 -0500

    Add payment collection actions

--- src/functions/onSalesWritten/index.ts
+++ src/functions/onSalesWritten/index.ts
@@ -1,6 +1,7 @@
 import { EventHandlerFunction } from '../../common/types/functions';
 import { Sale } from '../../common/types/sale';
 import { billingDocument } from './actionHandlers/billingDocumentActions';
+import { paymentCollection } from './actionHandlers/paymentCollectionActions';
 import { withholding } from './actionHandlers/withholdingActions';
 
 export const onSalesWrittenFunction: EventHandlerFunction = async (event) => {
@@ -64,4 +65,27 @@ const withholdingHandler: EventHandlerFunction = async (event) => {
 const paymentCollectionHandler: EventHandlerFunction = async (event) => {
   const beforeDoc = event.data?.before.data();
   const afterDoc = event.data?.after.data();
+
+  const beforePaymentCollectionDoc = beforeDoc?.paymentCollection;
+  const afterPaymentCollectionDoc = afterDoc?.paymentCollection;
+
+  if (!beforePaymentCollectionDoc && !afterPaymentCollectionDoc) {
+    console.log('No payment collection registered');
+    return;
+  }
+
+  if (!beforePaymentCollectionDoc) {
+    await paymentCollection.create(afterDoc as Sale);
+    return;
+  }
+
+  if (beforePaymentCollectionDoc && afterPaymentCollectionDoc) {
+    await paymentCollection.update(afterDoc as Sale);
+    return;
+  }
+
+  if (!afterPaymentCollectionDoc) {
+    await paymentCollection.remove(beforeDoc as Sale);
+    return;
+  }
 };
